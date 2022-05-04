fda5a6c104c89b4d0f4ed310b7373008d40d3e23
mstekachov <mstekachov@gmail.com>
2022-05-04 18:08:14 +0300

    local first commit

--- config/db_params.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Массив с параметрами подключения к базе данных
-return array(
-    'host' => 'localhost',
-    'dbname' => 'liderzm5_lider',
-    'user' => 'liderzm5_lider',
-    'password' => 'ji4khr%X',
-);
